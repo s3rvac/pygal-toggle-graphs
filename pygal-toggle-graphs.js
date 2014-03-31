2fc2b22ee9724d6077762b038729822596eafdaf
Petr Zemek <s3rvac@gmail.com>
2014-03-31 16:21:14 +0200

    Initial commit.

---
+++ pygal-toggle-graphs.js
@@ -0,0 +1,54 @@
+/**
+* pygal-toggle-graphs.js - Toggle the visibility of graphs in a pygal chart by
+*                          clicking on the legend.
+*
+* Copyright (c) 2014 Petr Zemek <s3rvac@gmail.com>
+*
+* Distributed under GNU LGPLv3:
+*
+*    This program is free software: you can redistribute it and/or modify it
+*    under the terms of the GNU General Public License as published by the Free
+*    Software Foundation, either version 3 of the License, or (at your option)
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful, but
+*    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+*    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+*    for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+(function() {
+	init = function(ctx) {
+		$vg('.activate-serie', ctx).click((function() {
+			var num = this.id.replace('activate-serie-', '');
+			var plot = $vg('.serie-' + num + ' .reactive', ctx);
+			var plotOverlay = $vg('.text-overlay .serie-' + num, ctx);
+			var rect = $vg(this).children('rect');
+			if (rect.css('fill') == 'transparent') {
+				// Show.
+				rect.css('fill', '');
+				plotOverlay.css('visibility', 'visible');
+				return plot.show();
+			} else {
+				// Hide.
+				rect.css('fill', 'transparent');
+				plotOverlay.css('visibility', 'hidden');
+				return plot.hide();
+			}
+		}));
+	};
+
+	$vg(function() {
+		var $charts = $vg('.pygal-chart');
+		if ($charts.size()) {
+			return $charts.each(function() {
+				return init($vg(this));
+			});
+		} else {
+			return init();
+		}
+	});
+}).call(this);
